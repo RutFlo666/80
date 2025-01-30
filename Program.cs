f075020a603075397a5bb1a8ed241a058c77f2e7
RutFlo666 <154447740+RutFlo666@users.noreply.github.com>
2025-01-30 11:29:07 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿
+namespace _170125
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string frNumber = Console.ReadLine();
+            float Fr = float.Parse(frNumber);
+
+
+            string scdNumber = Console.ReadLine();
+            float Scd = float.Parse(scdNumber);
+
+            string dog = Console.ReadLine();
+
+
+            if (dog == "+")
+            {
+                Console.WriteLine(Fr + Scd);
+
+            }
+            else if (dog == "-")
+            {
+                Console.WriteLine(Fr - Scd);
+            }
+            if (dog == "*")
+            {
+                Console.WriteLine(Fr * Scd);
+
+            }
+            else if (dog == "/")
+            {
+                Console.WriteLine(Fr / Scd);
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
